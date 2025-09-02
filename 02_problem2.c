c028846b20e4b339205730e4af00577f207b1746
Ishwarya S <sureshishwarya666@gmail.com>
2025-09-02 23:19:38 +0530

    Add files via upload

---
+++ 02_problem2.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(){
+    int r = 8;
+    int h = 12;
+    float area= 3.14*r*r;
+    float volume= 3.14*r*r*h;
+    printf("The area of the circle is %f \n",area);
+    printf("the volume of the cylinder is %f", volume);
+    return 0;
+}
